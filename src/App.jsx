69705db4b02167c5c5716d668d17b2e67264b612
amritansh-mishra <mamritansh1911@gmail.com>
2026-02-09 21:41:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './src/components/Navbar.jsx'
+import About from './src/components/About.jsx'
+import Contact from './src/components/Contact.jsx'
+import Footer from './src/components/Footer.jsx' 
+import Experience from './src/components/Experience.jsx'
+import Projects from './src/components/Projects.jsx'
+import Skills from './src/components/Skills.jsx'
+import Education from './src/components/Education.jsx'
+
+
+
+
+function app () {
+  return (
+    <div className='bg-[#]'>
+
+    </div>
+  )
+}
+
+export default app;
