9ca954a99a72eecddbae50f2d67b583788637d05
amritansh-mishra <mamritansh1911@gmail.com>
2026-02-18 09:25:47 +0530

    added box container and fixed the design

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -3,72 +3,98 @@ import { experiences } from "../../constants";
 
 const Experience = () => {
   return (
-   <section
-   id="experiences"
-   className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[20vw] font-sans bg-skills-gradient">
+    <section
+      id="experiences"
+      className="py-24 pb-24 px-[12vw] md:px-[7vw] lg:px-[20vw] font-sans bg-skills-gradient"
+    >
+      {/**Section title */}
 
-    {/**Section title */}
+      <div className=" text-center mb-10">
+        <h2 className="text-4xl text-white font-semibold ">EXPERIENCE</h2>
+        <div className="w-36 h-1 bg-[#fca311] mx-auto mt-1 rounded-full"></div>
+        <p className="text-white mt-4 text-lg font-semibold">
+          Heloo there I'm very unexperienced fellow
+        </p>
+      </div>
 
-    <div className=" text-center mb-10">
-      <h2 className="text-4xl text-white font-semibold ">
-        EXPERIENCE
-      </h2>
-      <div className="w-36 h-1 bg-[#fca311] mx-auto mt-1 rounded-full"></div>
-      <p className="text-white mt-4 text-lg font-semibold">
-        Heloo there I'm very unexperienced fellow 
-     </p>
-    </div>
-    {/**Experience timeline */}
-    <div className="relative">
+      {/**Experience timeline */}
 
-      {/** Vertical line */}
-      <div className=" absolute sm:left-1/2 left-0 transform -translate-x-1/2 sm:-translate-x-0 w-1 bg-white h-full"></div>
-         {/**Experience Enteries */}
-         {experiences.map((experience, index) => (
+      <div className="relative">
+        {/** Vertical line */}
+        <div className=" absolute sm:left-1/2 left-0 transform -translate-x-1/2 sm:-translate-x-0 w-1 bg-white h-full"></div>
+
+        {/**Experience Enteries */}
+        {experiences.map((experience, index) => (
           <div
             key={experience.id}
             className={`flex flex-col sm:flex-row items-center mb-16 ${
               index % 2 === 0 ? "sm:justify-end" : "sm:justify-start"
-            }`}>
-
+            }`}
+          >
             {/**Timeline circle */}
-            <div className="absolute sm:left-1/2 left-0 transform -translate-x-1/2 bg-gray-400 border-4 border-[#8245ec] w-12 h-12 sm:w-16 sm:h-16 rounded-full flex justify-center items-center z-10">
-              
-              <img 
-              src="" 
-              alt="" 
-              className="w-full h-full object-cover rounded-full"/> {/** experience.img and .company */}
-            
+            <div className="absolute sm:left-1/2 left-0 transform -translate-x-1/2 bg-gray-400 border-4 border-[#fca311] w-12 h-12 sm:w-16 sm:h-16 rounded-full flex justify-center items-center z-10">
+              <img
+                src=""
+                alt=""
+                className="w-full h-full object-cover rounded-full"
+              />{" "}
+              {/** experience.img and .company */}
             </div>
+            {/**Content Section */}
+            <div
+              className={`w-full sm:w-[45%] p-4 sm:p-8 rounded-2xl border border-white bg-gray-900 backdrop-blur-md shadow-[0_0_20px_1px_rgba(252,163,17,0.3)] transform transition-transform duration-300 hover:scale-105 ${
+                index % 2 === 0
+                  ? "sm:ml-auto" // even - pushes to LEFT
+                  : "sm:mr-auto" // odd - pushes to RIGHT
+              } ml-8 mr-8`}
+            >
+              {/** flex container for image and text */}
+              <div className=" flex items-center space-x-6">
+                {/**Compny logo */}
+                <div className="w-16 h-16 bg-white rounded-md overflow-hidden">
+                  <img
+                    className="w-full h-full object-cover"
+                    src=""
+                    alt={experience.company}
+                  />{" "}
+                  {/** experience.img .company */}
+                </div>
 
-            {/** Square box content section */}
-
-            <div className={`w-full sm:max-w-md p-4 sm:p-8 rounded-2xl shadow-2xl border border-white bg-gray-900 backdrop-blur-md shadow-[0_0_20px_1px_rgba(130,69,236,0.3)] ${
-                index % 2 === 0 ? "sm:ml-0" : "sm:mr-0"
-              } sm:ml-44 sm:mr-44 ml-8 transform transition-transform duration-300 hover:scale-105`}>
-
-            {/** flex container for image and text */}
-
-            <div className=" flex items-center space-x-6">
-
-              {/**Compny logo */}
-              <div className="w-16 h-16 bg-whiterounded-md overflow-hidden">
-                <img className="w-full h-full object-cover" src="" alt="" /> {/** experience.img .company */}
+                {/**Role,Company name, and Date */}
+                <div className="flex flex-col justify-between">
+                  <div>
+                    <h3 className="text-xl sm:text-2xl text-white font-semibold">
+                      {experience.role}
+                    </h3>
+                    <h4 className="text-md sm:text-sm text-gray-300">
+                      {experience.company}
+                    </h4>
+                    <h4 className="text-md sm:text-sm text-gray-400 mt-2">
+                      {experience.date}
+                    </h4>
+                  </div>
+                </div>
+                <p className="mt-4 text-gray-400">
+                  {experience.desc}
+                </p>
+                <div>
+                  <h5 className="font-medium text-white mt-4">Skills</h5>
+                  <ul>
+                    {experience.skills.map((skill, index) => (
+                      <li
+                        key={index}
+                        className="text-gray-300 px-4 py-1 text-xs sm:text-sm rounded-lg ">
+                        
+                      </li>
+                    ))}
+                  </ul>
+                </div>
               </div>
-
-
-      
             </div>
-
-            </div>
-
           </div>
-         ))}
-
-      
-
-    </div>
-   </section>
+        ))}
+      </div>
+    </section>
   );
 };
 
